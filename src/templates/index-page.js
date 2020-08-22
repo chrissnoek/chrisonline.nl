c35ed99542dc675ff8cda1530c71a5687c11a81c
Chris Snoek <chrissnoek@Chriss-MacBook-Pro.local>
2020-08-22 21:59:39 +0200

    change homepage

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,22 +1,25 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
 
-import Layout from '../components/Layout'
-import Features from '../components/Features'
-import BlogRoll from '../components/BlogRoll'
+import Layout from "../components/Layout";
+import Features from "../components/Features";
+import BlogRoll from "../components/BlogRoll";
 
 export const IndexPageTemplate = ({
-  image,
-  title,
-  heading,
-  subheading,
-  mainpitch,
-  description,
-  intro,
+	image,
+	title,
+	heading,
+	subheading,
+	mainpitch,
+	description,
+	intro,
 }) => (
-  <div>
-    <div
+	<div className="grid items-center justify-center h-screen">
+		<h1 className="text-white text-3xl font-bold">
+			Chris <span className="text-teal-400">Online</span>
+		</h1>
+		{/* <div
       className="full-width-image margin-top-0"
       style={{
         backgroundImage: `url(${
@@ -110,84 +113,84 @@ export const IndexPageTemplate = ({
           </div>
         </div>
       </div>
-    </section>
-  </div>
-)
+    </section> */}
+	</div>
+);
 
 IndexPageTemplate.propTypes = {
-  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  title: PropTypes.string,
-  heading: PropTypes.string,
-  subheading: PropTypes.string,
-  mainpitch: PropTypes.object,
-  description: PropTypes.string,
-  intro: PropTypes.shape({
-    blurbs: PropTypes.array,
-  }),
-}
+	image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+	title: PropTypes.string,
+	heading: PropTypes.string,
+	subheading: PropTypes.string,
+	mainpitch: PropTypes.object,
+	description: PropTypes.string,
+	intro: PropTypes.shape({
+		blurbs: PropTypes.array,
+	}),
+};
 
 const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
+	const { frontmatter } = data.markdownRemark;
 
-  return (
-    <Layout>
-      <IndexPageTemplate
-        image={frontmatter.image}
-        title={frontmatter.title}
-        heading={frontmatter.heading}
-        subheading={frontmatter.subheading}
-        mainpitch={frontmatter.mainpitch}
-        description={frontmatter.description}
-        intro={frontmatter.intro}
-      />
-    </Layout>
-  )
-}
+	return (
+		<Layout>
+			<IndexPageTemplate
+				image={frontmatter.image}
+				title={frontmatter.title}
+				heading={frontmatter.heading}
+				subheading={frontmatter.subheading}
+				mainpitch={frontmatter.mainpitch}
+				description={frontmatter.description}
+				intro={frontmatter.intro}
+			/>
+		</Layout>
+	);
+};
 
 IndexPage.propTypes = {
-  data: PropTypes.shape({
-    markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.object,
-    }),
-  }),
-}
+	data: PropTypes.shape({
+		markdownRemark: PropTypes.shape({
+			frontmatter: PropTypes.object,
+		}),
+	}),
+};
 
-export default IndexPage
+export default IndexPage;
 
 export const pageQuery = graphql`
-  query IndexPageTemplate {
-    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
-      frontmatter {
-        title
-        image {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        heading
-        subheading
-        mainpitch {
-          title
-          description
-        }
-        description
-        intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-            text
-          }
-          heading
-          description
-        }
-      }
-    }
-  }
-`
+	query IndexPageTemplate {
+		markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+			frontmatter {
+				title
+				image {
+					childImageSharp {
+						fluid(maxWidth: 2048, quality: 100) {
+							...GatsbyImageSharpFluid
+						}
+					}
+				}
+				heading
+				subheading
+				mainpitch {
+					title
+					description
+				}
+				description
+				intro {
+					blurbs {
+						image {
+							childImageSharp {
+								fluid(maxWidth: 240, quality: 64) {
+									...GatsbyImageSharpFluid
+								}
+							}
+						}
+						text
+					}
+					heading
+					description
+				}
+			}
+		}
+	}
+`;
