d61a136adcfc5ea5cea97013ae9541a32943cc8f
Chris Snoek <chrissnoek8@hotmail.com>
2020-09-02 15:40:30 +0200

    add new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
 	},
 	purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
 	theme: {
+		colors: {
+			blue: {
+				light: '#0bb5d6',
+				dark: '#2369b3',
+			}
+		},
 		extend: {},
 	},
 	variants: {},
