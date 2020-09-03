766c07aa6994165c3ea315d94adf3056184bb307
Chris Snoek <chrissnoek@Chriss-MacBook-Pro.local>
2020-09-03 08:12:29 +0200

    change tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,10 @@ module.exports = {
 	purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
 	theme: {
 		colors: {
-			blue: {
-				light: '#0bb5d6',
-				dark: '#2369b3',
-			}
+			logo: {
+				light: "#0bb5d6",
+				dark: "#2369b3",
+			},
 		},
 		extend: {},
 	},
